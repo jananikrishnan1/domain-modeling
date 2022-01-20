333e8a0d94f8dd3663c98e1433edbc8260d3f09d
jananikrishnan1 <janani.krishnan@kellogg.northwestern.edu>
2022-01-20 02:49:25 +0000

    Class Exercise 1_revised

--- crm/crm.sql
+++ crm/crm.sql
@@ -5,36 +5,41 @@
 -- CREATE TABLES
 
 CREATE TABLE contacts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT
-    first_name TEXT
-    last_name TEXT
-    title TEXT
-    email TEXT
-    phone_number TEXT
-    company_id TEXT 
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    title TEXT,
+    email TEXT,
+    phone_number TEXT,
+    company_id INTEGER, 
     );
 
 CREATE TABLE activity (
-    id INTEGER PRIMARY KEY AUTOINCREMENT
-    created_at TEXT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at TEXT,
     notes TEXT,
     salesperson_id INTEGER,
-    contact_id INTEGER
+    contact_id INTEGER,
 );
 
 CREATE TABLE companies (
-    id INTEGER PRIMARY KEY AUTOINCREMENT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT
 );
 
 CREATE TABLE industries (
-    id INTEGER PRIMARY KEY AUTOINCREMENT
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT 
 );
 
 CREATE TABLE salespeople
-    id INTEGER PRIMARY KEY AUTOINCREMENT
-    company_id INTEGER
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_id INTEGER,
     industry_id INTEGER
     );
 
+CREATE TABLE industry_membership
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_id INTEGER,
+    industry_id INTEGER
+    );
