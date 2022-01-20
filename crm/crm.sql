d4b9c19db6bdd040cfe17990237c62cc5c40ba0b
jananikrishnan1 <janani.krishnan@kellogg.northwestern.edu>
2022-01-20 02:44:20 +0000

    Class Exercise 1

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,38 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT
+    first_name TEXT
+    last_name TEXT
+    title TEXT
+    email TEXT
+    phone_number TEXT
+    company_id TEXT 
+    );
+
+CREATE TABLE activity (
+    id INTEGER PRIMARY KEY AUTOINCREMENT
+    created_at TEXT
+    notes TEXT,
+    salesperson_id INTEGER,
+    contact_id INTEGER
+);
+
+CREATE TABLE companies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT
+    name TEXT
+);
+
+CREATE TABLE industries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT
+    name TEXT 
+);
+
+CREATE TABLE salespeople
+    id INTEGER PRIMARY KEY AUTOINCREMENT
+    company_id INTEGER
+    industry_id INTEGER
+    );
+
